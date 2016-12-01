6748f7593135487835153ee8d5e5e12880d54f73
honkeechan <honkeechan@hotmail.com>
2016-12-01 17:34:01 +0800

    创建server

---
+++ server/tornado_server.py
@@ -0,0 +1,26 @@
+# encoding: utf8
+import tornado.ioloop
+import tornado.web
+import tornado.escape
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hello world")
+
+    def post(self):
+        args = self.request.arguments
+        print args
+        self.write(tornado.escape.json_encode(args))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/test", TestHandler),
+    ])
+
+def tornadoThread():
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    tornadoThread()
